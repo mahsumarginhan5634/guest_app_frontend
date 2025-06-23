334415b085f38a183ff766a2c43558fe829d714d
mahsumarginhan5634 <arginhanibrahim56@gmail.com>
2025-06-23 14:22:55 +0300

    followed-birthdays

--- src/components/HomeContainer/RightSidebarContainer/Birthdays.jsx
+++ src/components/HomeContainer/RightSidebarContainer/Birthdays.jsx
@@ -1,49 +1,62 @@
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import {getUserFromLocalStorage} from "../../../store/localStorage.js";
+import {useEffect, useState} from "react";
+import {getMyFollowedsBirthdays} from "../../../services/UserFollowService.js";
 export default function Birthdays(){
+
+    const user = getUserFromLocalStorage();
+    const [myFollowingsBirthdays , setMyFollowingsBirthdays] = useState([]);
+
+    const getMyFollowingBirthdays = async (userId) => {
+        const tempResponse = await getMyFollowedsBirthdays(userId);
+        if(tempResponse.status){
+            const response = await tempResponse.json();
+            setMyFollowingsBirthdays(response.data);
+        }
+    }
+
+    useEffect(() => {
+        if(user){
+            const executeThisMethod = async () => {
+                await getMyFollowingBirthdays(user?.id);
+            }
+            executeThisMethod();
+        }
+    }, []);
+
     return (
         <>
-            <div className="bg-white rounded-lg shadow p-4 mb-6">
-                <h3 className="font-bold text-lg mb-4">Doğum Günleri</h3>
-                <div className="flex items-center space-x-3 mb-3">
-                    <div className="bg-blue-100 p-2 rounded-full">
-                        <svg
-                            className="w-6 h-6 text-blue-600"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z"
-                                clipRule="evenodd"
-                            ></path>
-                            <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path>
-                        </svg>
+            {
+                myFollowingsBirthdays.length > 0 &&
+                (
+                    <div className="bg-white rounded-lg shadow p-4 mb-6">
+                        <h3 className="font-bold text-lg mb-4">Doğum Günleri</h3>
+                        <div className="flex space-x-3 mb-3 flex-col gap-y-3 justify-items-center">
+                            {
+                                myFollowingsBirthdays.map((myFollowing, index) => (
+                                    <div
+                                        className={"flex flex-row items-center  p-2 gap-x-2 bg-gray-200 rounded-lg  w-full"}
+                                        key={index}>
+                                        <div className="bg-blue-100 p-2 rounded-full">
+                                            <CardGiftcardIcon
+                                                className="w-6 h-6 text-blue-600"
+                                            />
+                                        </div>
+                                        <p className="text-sm">
+                                            Bugün
+                                            <span className="font-semibold">
+                                        {" " + myFollowing.name.substring(0, 1).toUpperCase() + myFollowing.name.substring(1)}
+                                                {" " + myFollowing.surname.substring(0, 1).toUpperCase() + myFollowing.surname.substring(1)}
+
+                                    </span>'in doğum günü.
+                                        </p>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
-                    <p className="text-sm">
-                        Bugün <span className="font-semibold">Ayşe Demir</span>'in doğum günü.
-                    </p>
-                </div>
-                <div className="flex items-center space-x-3">
-                    <div className="bg-blue-100 p-2 rounded-full">
-                        <svg
-                            className="w-6 h-6 text-blue-600"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z"
-                                clipRule="evenodd"
-                            ></path>
-                            <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path>
-                        </svg>
-                    </div>
-                    <p className="text-sm">
-                        Yarın <span className="font-semibold">Burak Şahin</span>'in doğum günü.
-                    </p>
-                </div>
-            </div>
+                )
+            }
         </>
     );
 }
